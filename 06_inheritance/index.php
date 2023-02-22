9778cb8a87760f6f423d986aee9591af45ac6eca
M. Mustakim Surya <muhammadmustakimsurya@gmail.com>
2023-02-22 23:27:02 +0700

    extends inheritance

--- 06_inheritance/index.php
+++ 06_inheritance/index.php
@@ -1,29 +1,20 @@
 <?php
 
 class Produk {
-    public $judul, $penulis, $penerbit, $harga, $jmlHal, $wktMain, $tipe;
+    public $judul, $penulis, $penerbit, $harga, $jmlHal, $wktMain;
 
-    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHal = 0, $wktMain = 0, $tipe=""){
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHal = 0, $wktMain = 0){
         $this->judul = $judul;
         $this->penulis = $penulis;
         $this->penerbit = $penerbit;
         $this->harga = $harga;
         $this->jmlHal = $jmlHal;
         $this->wktMain = $wktMain;
-        $this->tipe = $tipe;
 
     }
 
     public function getLabel(){
         return "$this->penulis, $this->penerbit";
-    }
-
-    public function getIngfoLengkap(){
-        if($this->tipe=="komik"){
-            return "Komik : $this->judul | $this->penulis, $this->penerbit ($this->harga) - $this->jmlHal Halaman.";
-        } else {
-            return "Game : $this->judul | $this->penulis, $this->penerbit ($this->harga) - $this->wktMain Jam.";
-        }
     }
 }
 
@@ -34,9 +25,21 @@ class CetakInfoProduk {
     }
 }
 
-$produk1 = new Produk("Naruto", "Mashasi Kisimoto", "Shonen Jump", 30000, 100, 0, "komik");
-$produk2 = new Produk("Vinland Saga", "Joko Widodo", "Gramedia", 22000, 245, 0, "komik");
-$produk3 = new Produk("GTA 5", "Kalpin Batagor", "Xijinping Studio", 265000, 0, 45, "game");
+class Komik extends Produk {
+    public function getIngfoLengkap(){
+        return "Komik : $this->judul | $this->penulis, $this->penerbit ($this->harga) - $this->jmlHal Halaman.";
+    }
+}
+
+class Game extends Produk {
+    public function getIngfoLengkap(){
+        return "Game : $this->judul | $this->penulis, $this->penerbit ($this->harga) - $this->wktMain Jam.";
+    }
+}
+
+$produk1 = new Komik("Naruto", "Mashasi Kisimoto", "Shonen Jump", 30000, 100, 0);
+$produk2 = new Komik("Vinland Saga", "Joko Widodo", "Gramedia", 22000, 245, 0);
+$produk3 = new Game("GTA 5", "Kalpin Batagor", "Xijinping Studio", 265000, 0, 45);
 
 echo $produk1->getIngfoLengkap();
 echo "<br>";
