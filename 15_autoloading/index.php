dbaf80c98ba56846ba4f2a0977159a800eb80300
M. Mustakim Surya <muhammadmustakimsurya@gmail.com>
2023-02-23 02:43:57 +0700

    autoload

---
+++ 15_autoloading/index.php
@@ -0,0 +1,9 @@
+<?php
+// dapat dilakukan dengan beberapa cara
+// 1. semua line require banyak-banyak yang digabungkan pada 1 file kemudian yang dipanggil file itu saja
+// 2. autoload semua class dengan spl_autoload_register();
+
+spl_autoload_register(function($class){
+    require_once __DIR__ . 'src/' . $class . '.php'
+})
+// nama direktori src hanya contoh saja
